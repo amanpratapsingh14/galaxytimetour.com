9eb4618e4dcb28fc5866d19ff184866f6566c0fb
Aman Pratap Singh <88184752+amanpratapsingh14@users.noreply.github.com>
2025-01-01 13:11:27 +0530

    Update SingleDestination.tsx

--- src/components/Destination/SingleDestination.tsx
+++ src/components/Destination/SingleDestination.tsx
@@ -2,15 +2,21 @@ import { Destination } from "@/types/destination";
 import Image from "next/image";
 import Link from "next/link";
 
+
 const SingleDestination = ({ destination }: { destination: Destination }) => {
   const { title, image, paragraph } = destination;
   return (
     <div className="wow fadeInUp -mt-10 mb-6 justify-between hover:shadow-two dark:hover:shadow-gray-dark group relative overflow-hidden rounded-lg bg-white shadow-one duration-300 dark:bg-dark"
       data-wow-delay=".1s">
       <Link href={`/destination/${title}`} className="relative block aspect-[37/22] w-full">
-        <Image src={image} alt="image" fill
+        <Image 
+          src={image} 
+          alt="image" 
+          fill
+          style={{ objectFit: 'cover' }}
           sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-          style={{ objectFit: 'cover' }}/>
+          priority
+        />
       </Link>
       <div className="p-6 sm:p-8 md:px-6 md:py-8 lg:p-8 xl:px-5 xl:py-8 2xl:p-8">
         <h2>
